22305ecc27fcf4e42979fdf9850f235460e8f58c
Anton Porodnikov <antonporonikov@outlook.com>
2023-05-24 14:23:31 +0300

    v2.0.2

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <cmath>
-
 namespace geo {
 
 struct Coordinates {
@@ -19,18 +17,6 @@ struct Coordinates {
     }
 };
 
-inline double ComputeDistance(Coordinates from, Coordinates to)
-{
-    using namespace std;
+double ComputeDistance(Coordinates from, Coordinates to);
 
-    if (from == to)
-    {
-        return 0;
-    }
-
-    static const double dr = 3.1415926535 / 180.;
-    return acos(sin(from.lat * dr) * sin(to.lat * dr) + cos(from.lat * dr) *
-        cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr)) * 6371000;
-}
-
-}
+}  // namespace geo
